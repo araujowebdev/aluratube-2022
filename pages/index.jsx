5e19fbe955239d638232ab763d752c07b61534c9
Raphael Araujo <araujowebdev@gmail.com>
2022-11-11 10:39:18 -0300

    aula 03

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,20 +1,14 @@
 import React from "react";
 import config from "../config.json";
 import styled from "styled-components";
-import { CSSReset } from "../src/components/CSSReset";
 import Menu from "../src/components/Menu";
 import { StyledTimeline } from "../src/components/Timeline";
 
 function HomePage() {
-  const estilosDaHomePage = {
-    // backgroundColor: "red"
-  };
-
   const [valorDoFiltro, setValorDoFiltro] = React.useState("");
 
   return (
     <>
-      <CSSReset />
       <div
         style={{
           display: "flex",
@@ -48,11 +42,14 @@ export default HomePage;
 // }
 
 const StyledHeader = styled.div`
+  background-color: ${({ theme }) => theme.backgroundLevel1};
+
   img {
     width: 80px;
     height: 80px;
     border-radius: 50%;
   }
+
   .user-info {
     display: flex;
     align-items: center;
@@ -66,6 +63,7 @@ const StyledBanner = styled.div`
   background-color: royalblue;
   background-image: url(${({ bg }) => bg});
   /* background-image: url(${config.bg}); */
+  height: 230px;
 `;
 
 function Header() {
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import DarkModeSwitch from "./components/DarkModeSwitch";
 import Search from "./components/Search";
 
 const StyledMenu = styled.header`
@@ -38,6 +39,7 @@ export default function Menu({ valorDoFiltro, setValorDoFiltro }) {
         valorDoFiltro={valorDoFiltro}
         setValorDoFiltro={setValorDoFiltro}
       />
+      <DarkModeSwitch />
     </StyledMenu>
   );
 }
